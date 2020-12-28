a3fc0545f71f31ea162da2202bc83c6322bdc849
akshat3358 <akshatchaturvedi422@gmail.com>
2020-12-28 11:02:05 +0530

    Reverse the L.L.

---
+++ LinkedList/ReverseLinkedList.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+struct node
+{
+	int info;
+	struct node *link;
+};
+struct node *reverse(struct node *start)
+{
+	struct node *prev, *ptr, *next;
+	prev=NULL;
+   	ptr=start;
+	while(ptr!=NULL)
+	{
+		next=ptr->link;
+		ptr->link=prev;
+		prev=ptr;
+		ptr=next;
+	}
+	start=prev;
+	return start;
