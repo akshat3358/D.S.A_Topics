9a08ea3cff281f4376c4a55c67689905e4887c76
akshat3358 <akshatchaturvedi422@gmail.com>
2020-12-28 10:58:30 +0530

    Delete element from L.L.

---
+++ LinkedList/DeleteAnElement.c
@@ -0,0 +1,38 @@
+#include<stdlib.h>
+struct node
+{
+	int info;
+	struct node *link;
+};
+struct node *del(struct node *start,int data)
+{
+	struct node *tmp,*p;
+	if(start==NULL)
+	{
+		printf("List is empty\n");
+		return start;
+	}
+	/*Deletion of first node*/
+	if(start->info==data)
+	{
+		tmp=start;
+		start=start->link;  
+		free(tmp);
+		return start;
+	}
+	/*Deletion in between or at the end*/
+	p=start;
+	while(p->link!=NULL)
+	{
+		if(p->link->info==data)   
+		{
+			tmp=p->link;
+			p->link=tmp->link;
+			free(tmp);
+			return start;
+		}
+		p=p->link;
+	}
+	printf("Element %d not found\n",data);
+	return start;
+}
