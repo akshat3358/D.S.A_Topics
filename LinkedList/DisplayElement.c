61cf18e05ed7b492e8b11449523f6c2b44a618f3
akshat3358 <akshatchaturvedi422@gmail.com>
2020-12-28 11:02:58 +0530

    Traverse the Linked List and display

---
+++ LinkedList/DisplayElement.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+struct node
+{
+	int info;
+	struct node *link;
+};
+void display(struct node *start)
+{
+	struct node *p;
+	if(start==NULL)
+	{
+		printf("List is empty\n");
+		return;
+	}
+	p=start;
+	printf("List is :\n");
+	while(p!=NULL)
+	{
+		printf("%d ",p->info);
+		p=p->link;
+	}
+	printf("\n\n");
+}
